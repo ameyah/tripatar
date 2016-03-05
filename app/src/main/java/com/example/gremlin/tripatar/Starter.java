c974b11e1e08fa4c3e3d8957ad2f956993be26c2
Chirag Sanghvi <csanghvi@appacitive.com>
2016-03-05 15:42:45 -0800

    Downgraded android version to 22 and initialised parse sdk in starter

--- app/src/main/java/com/example/gremlin/tripatar/Starter.java
+++ app/src/main/java/com/example/gremlin/tripatar/Starter.java
@@ -1,7 +1,14 @@
 package com.example.gremlin.tripatar;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseException;
 
 public class Starter extends AppCompatActivity {
 
@@ -9,5 +16,10 @@ public class Starter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter);
+
+        Resources res = getResources();
+
+        Parse.initialize(this, res.getString(R.string.parse_app_id), res.getString(R.string.parse_client_key));
+
     }
 }
